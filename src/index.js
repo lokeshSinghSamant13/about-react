7d3e0c7c19fde6fb318ad3c464d91b46c1aace5a
Lokesh Singh Samant <lokeshsinghsamant@gmail.com>
2022-07-14 08:57:23 +0530

    replace react redux with react context

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import { Provider } from "react-redux";
+// import { combineReducers, createStore } from "redux";
+import { BrowserRouter } from "react-router-dom";
+import ProductsProvider from "./context/product-context";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import "./index.css";
+import App from "./App";
+// import productReducer from "./store/reducers/products";
+
+// const rootReducer = combineReducers({
+//   shop: productReducer,
+// });
+
+// const store = createStore(rootReducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <ProductsProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductsProvider>
+  // <Provider store={store}>
+  //   <BrowserRouter>
+  //     <App />
+  //   </BrowserRouter>
+  // </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
