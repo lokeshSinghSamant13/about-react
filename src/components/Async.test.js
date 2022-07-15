1f7cfbd8537a3e05eddf445b7f0bdea231f46a53
Lokesh Singh Samant <lokeshsinghsamant@gmail.com>
2022-07-15 08:58:27 +0530

    using basic react testing features

---
+++ src/components/Async.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import Async from "./Async";
+
+describe("Async component", () => {
+  test("renders post if request succeeds", async () => {
+    // Using Mock Function
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First post" }],
+    });
+    render(<Async />);
+
+    const listItemElements = await screen.findAllByRole(
+      "listitem"
+      //   {},
+      //   { timeout: 2000 }
+    );
+    expect(listItemElements).not.toHaveLength(0);
+  });
+});
--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import Output from "./Output";
+
 const Greeting = () => {
+  const [changedTest, setChangedText] = useState();
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>It's good to see you!</p>
+      {!changedTest && <Output>It's good to see you!</Output>}
+      {changedTest && <Output>Changed!</Output>}
+      <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
 };
